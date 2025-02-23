d6c54adf9579a757cf57373249afc5cde753cf1b
rahul <rahul@Asus>
2025-02-23 12:21:18 -0500

    code update

---
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest2.java
@@ -0,0 +1,14 @@
+package travis_ci_tutorial_java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleCalculatorTest2 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
